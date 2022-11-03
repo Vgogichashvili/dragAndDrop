4ef5b507a1be9b9c976d682fe8f954ebda9d25d9
vgogichashvili47@gmail.com <98263919+Vgogichashvili@users.noreply.github.com>
2022-11-03 11:24:53 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dragAndDrop';
+
+   timePeriods = [
+    '0001',
+    '0002',
+    '0003',
+    '0004',
+    '0005',
+    '0006',
+    '0007',
+    '0008',
+    '0009',
+    '0010',
+    '0011',
+    '0012',
+    '0013',
+    '0014',
+    '0015',
+    '0016'
+    
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
+  }
 }
